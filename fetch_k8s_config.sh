9b48b3d42f240c1178034d0634440b93bb513b6c
Nic Jackson <jackson.nic@gmail.com>
2017-10-22 15:02:33 +0100

    Updated script to fetch k8s config

--- fetch_k8s_config.sh
+++ fetch_k8s_config.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# Check ip for cluster is in known hosts and add if neccessary
-if [ -z "`ssh-keygen -F $1`" ]; then
-  ssh-keyscan -H $1 >> ~/.ssh/known_hosts
-else
+# Check ip for cluster is in known hosts and remove
+if [ "`ssh-keygen -F $1`" ]; then
   ssh-keygen -R $1
-  ssh-keyscan -H $1 >> ~/.ssh/known_hosts
 fi
- 
+
+# Add new address to known hosts
+ssh-keyscan -H $1 >> ~/.ssh/known_hosts
+
 # Fetch kube config from master
 echo "Fetching Kubernetes config from remote server"
 ssh -i $2 -o BatchMode=yes ubuntu@$1 'cat /home/ubuntu/.kube/config' > kube.config
 
 # Parse kube config and extract certificates, certificates are base64 encoded
 cat kube.config | sed -n -E 's/certificate-authority-data: "(.*)"/\1/p' | sed -e 's/^[ \t]*//' | base64 -D > cluster-ca-cert.pem
-
 cat kube.config | sed -n -E 's/client-certificate-data: "(.*)"/\1/p' | sed -e 's/^[ \t]*//' | base64 -D > client-cert.pem
-
 cat kube.config | sed -n -E 's/client-key-data: "(.*)"/\1/p' | sed -e 's/^[ \t]*//' | base64 -D > client-key.pem
